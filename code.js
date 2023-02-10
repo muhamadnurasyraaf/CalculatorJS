e48cdecf98e3ecde605f51d1af63e3ace4222d00
Muhamad Nur Asyraaf <masyraaf14@gmail.com>
2023-02-11 03:10:00 +0800

    latest

---
+++ code.js
@@ -0,0 +1,19 @@
+const display = document.querySelector("#display");
+const button = document.querySelectorAll("button");
+
+button.forEach((btn) =>{
+    btn.onclick = () =>{
+        if(btn.id === "="){
+            display.value = eval(display.value);
+        }
+        else if(btn.id === "ac"){
+            display.value = "";
+        }
+        else if(btn.id == "de"){
+            display.value = display.value.slice(0,-1);
+        }
+        else{
+            display.value += btn.id;
+        }
+    }
+})
